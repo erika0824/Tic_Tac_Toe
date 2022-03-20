290e03567ab62f52faec91e985da24ec3f776291
Erika Huber <48628911+erika0824@users.noreply.github.com>
2022-03-19 21:32:26 -0500

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // DONE Create a tic-tac-toe grid using your HTML element of choice. When a cell in the grid is clicked, 
 //an X or O should appear in that spot depending on whose turn it is.
 
-// When a player has won, or the board is full and the game results in a draw, 
+//DONE When a player has won, or the board is full and the game results in a draw, 
 //a Bootstrap alert or similar Bootstrap component should appear across the screen announcing the winner.
 
 //DONE A button should be available to clear the grid and restart the game.
@@ -51,56 +51,47 @@ function switchTurns() {
        if(turns % 2 == 0) {
            $(this).text(player1);
            player1Scores.push(this.id);
-           
-           let win = checkWinner('PLAYER 1', player1Scores);
-           checkTie(turns, win);
+           checkWinner('PLAYER 1', player1Scores);
+           checkTie(turns);
            turns++;
-           console.log(turns)
            $('.alert-turn').text(`Player 2's turn`);
        } else {
            $(this).text(player2);
            player2Scores.push(this.id);
-           
-           let win = checkWinner('PLAYER 2', player2Scores);
-           checkTie(turns, win);
+           checkWinner('PLAYER 2', player2Scores);
+           checkTie(turns);
            turns++;
-           console.log(turns)
            $('.alert-turn').text(`Player 1's turn`)
        }
    })
 }
 
 function checkWinner(player, playerScores){
-    if(playerScores.length >= 2) {
+    if(playerScores.length > 2 && playerScores.length < 5) {
         for(let i = 0; i < winningCombos.length; i++) {
             if(winningCombos[i].every(elem => playerScores.includes(elem))) {
                 endGame(player);
                 declareWinner(player);
-                return 1;
             }  
         } 
     } 
-    return 0;
 } 
 
 
-
-function checkTie(squaresPlayed, win) {
-    console.log(squaresPlayed)
-    if(squaresPlayed == 8 && win == 0) {
+function checkTie(squaresPlayed) {
+    if(squaresPlayed == 8) {
         console.log(`It's a tie`);
-       document.querySelector('.end-game').style.display = "block";
-       document.querySelector('.end-game .text').textContent = `It's a tie`;
-       endGame();
+        document.querySelector('.end-game').style.display = "block";
+        document.querySelector('.end-game .text').textContent = `It's a tie`;
+        endGame();  
+        }
     }
-}
 
 function endGame(player) {
     //turn off event listener
     cells.off('click');
     player1Scores = [];
     player2Scores = [];
-    // declareWinner(player)
 }
 
 function declareWinner(winner) {
